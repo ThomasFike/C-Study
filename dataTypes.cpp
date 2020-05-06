fc81559459fe1cd4aea02ee2ba0931b18bfeaf2a
Thomas Fike <fike@me.com>
2020-05-06 09:18:53 -0700

    Hour One

---
+++ dataTypes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /* Character: char
+    *  Only one character
+    */
+    char grade = 'A';
+   
+    /* String: string
+    * Multiple chars
+    */
+    string phrase = "Giraffe Academy";
+
+    /* Integer: int
+     * Whole Numbers
+     */
+    int age = 50;
+
+    /* Floating Point: float
+     * Numbers with decimal points
+     */
+    float foo = 10.5;
+
+    /* Double Percision Floating Point: double
+     * double percision version of float
+     */
+    double gpa = 2.3;
+
+    /* Boolean: bool
+     * True or False
+     */
+    bool isMale = false;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    cout << "Bill" << endl;
+    return 0;
+}
