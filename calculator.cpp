60f8a10bea25b3ba342d18015fb1abdf051a52a9
Thomas Fike <fike@me.com>
2020-05-06 13:21:33 -0700

    Added better calculator

--- calculator.cpp
+++ calculator.cpp
@@ -2,15 +2,50 @@
 
 using namespace std;
 
+char getOpperation();
+double getNumber(int numberToGet);
+double computeResult(char opperation, double num1, double num2);
+
 int main() {
-    int num1, num2;
+    double num1, num2;
+    char opperation;
+    double result;
 
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter second number: ";
-    cin >> num2;
+    num1 = getNumber(1);
+    opperation = getOpperation();
+    num2 = getNumber(2);
 
-    cout << num1 + num2 << endl;
+    result = computeResult(opperation, num1, num2);
+    cout << "Computing " << num1 << opperation << num2 << endl;
+    cout << "Result: " << result << endl;
+    return 0;
+}
 
+char getOpperation() {
+    char operation;
+    cout << "Operation (+,-,*,/): ";
+    cin >> operation;
+    return operation;
+}
+
+double getNumber(int numberToGet) {
+    double num;
+    cout << "Enter number " << numberToGet << ": ";
+    cin >> num;
+    return num;
+}
+
+double computeResult(char opperation, double num1, double num2) {
+    double result;
+    if (opperation == '+')
+        return num1 + num2;
+    else if (opperation == '-')
+        return num1 + num2;
+    else if (opperation == '*')
+        return num1 * num2;
+    else if(opperation == '/')
+        return num1 / num2;
+    else
+        cout << "Invalid Operation";
     return 0;
 }
